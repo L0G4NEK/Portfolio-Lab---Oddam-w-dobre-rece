0f2e9556b20064e799c77c8c4b400dbc4fd51d8c
L0G4NEK <darekw132@gmail.com>
2023-01-10 19:14:22 +0100

    basic construction

---
+++ src/components/Home.js
@@ -0,0 +1,6 @@
+import HomeHeader from "./components/HomeHeader";
+import HomeThreeColumns from "./components/HomeThreeColumns";
+import HomeSimpleSteps from "./components/HomeSimpleSteps";
+import HomeAboutUs from "./components/HomeAboutUs";
+import HomeWhoWeHelp from "./components/HomeWhoWeHelp";
+import HomeContact from "./components/HomeContact";
