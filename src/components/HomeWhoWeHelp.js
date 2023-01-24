8f28c75ca6a255882b4003ea4e64cc4c9752b6bc
L0G4NEK <darekw132@gmail.com>
2023-01-24 16:06:34 +0100

    filtred data

--- src/components/HomeWhoWeHelp.js
+++ src/components/HomeWhoWeHelp.js
@@ -6,6 +6,7 @@ import db from '../firebase'
 const HomeWhoWeHelp = () => {
 
     const [info , setInfo] = useState([]);
+    const [filtredInfo , setFiltredInfo] = useState([]);
     
     useEffect(() => {
         db.collection("Organization").onSnapshot((snapshot) => {
@@ -21,8 +22,34 @@ const HomeWhoWeHelp = () => {
         });
     }, []);
 
-    const handleClick = () => {
-        console.log(info.type)
+//     const filterQuery = {
+//     language: "French"
+// }
+
+// function parseUserData(data) {
+//     return data.filter(item =>
+//         Object.keys(filterQuery).every(key => item[key] === filterQuery[key])
+//     ).map(({id, firstName, lastName, email, nin}) => ({
+//         id: id,
+//         fullName: `${firstName} ${lastName}`,
+//         email: email,
+//         nin: nin
+//     })).sort((a, b) => a.nin - b.nin);
+// }
+
+// console.log(parseUserData(users))
+
+    const handleClick = (org) => {
+        let filtredInfo = info.filter(item => item.type === org)
+        setFiltredInfo(
+            filtredInfo.map((doc) => ({
+                id: doc.id,
+                name: doc.name,
+                type: doc.type,   
+                description: doc.description,
+                examples: doc.examples,
+            }))
+        )
     }
 
     return ( 
@@ -30,11 +57,11 @@ const HomeWhoWeHelp = () => {
             <h2>Komu pomagamy?</h2>
             <img src="../assets/Decoration.png" alt="decoration" className="homeWhoWeHelp__decoration" />
             <div className="homeWhoWeHelp__organizations">
-                <button className="fundation" onClick={handleClick('fundation')}>Fundacjom</button>
-                <button className="organization" onClick={handleClick('organization')}>Organizacjom Pozarządowym</button>
-                <button className="localCollections" onClick={handleClick('localCollection')}>Lokalnym Zbiórkom</button>
+                <button className="fundation" onClick={() => handleClick('fundation')}>Fundacjom</button>
+                <button className="organization" onClick={() => handleClick('organization')}>Organizacjom Pozarządowym</button>
+                <button className="localCollections" onClick={() => handleClick('local')}>Lokalnym Zbiórkom</button>
             </div>
-            {info?.map(({ id, name, type, description, examples }) => (
+            {filtredInfo?.map(({ id, name, type, description, examples }) => (
                 <div key={id}>
                     <h2>name: {name}</h2>
                     <h2>country: {type}</h2>
